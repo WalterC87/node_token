b492055385fa6d53b39ac8944217a7d86c26df88
unknown <30202775@gua-hcp-lt258.acshcp.com>
2016-03-03 17:14:07 -0600

    create structure to token test

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -3,8 +3,9 @@ var service = require('./service');
 
 exports.emailSignUp = function(req, res, next){
 	models.User.create({
-		loginUser: req.body.loginUser,
-		password: req.body.password
+		loginUser: req.body.loginUser.toLowerCase(),
+		password: req.body.password,
+		token: Math.random().toString(32).substring(2);
 	}).then(function (user){
 		if(!user){
 			res.status(500);
@@ -17,12 +18,31 @@ exports.emailSignUp = function(req, res, next){
 			res.json({
 				type: true,
 				data: user
-			})
+			});
 			res.send({token: service.createToken(user)});
 		}
 	});
 }
 
 exports.emailLogin = function(req, res, next){
-	
+	models.User.findOne({
+		where: {
+			loginUser: req.body.loginUser.toLowerCase()
+		}
+	}).then(function (user){
+		if(!user){
+			res.status(500);
+			res.json({
+				type: false,
+				data: "error: " + user
+			});
+		}else{
+			res.status(200);
+			res.json({
+				type: true,
+				data: user
+			});
+			res.send({token: service.createToken(user)});
+		}
+	})
 }
